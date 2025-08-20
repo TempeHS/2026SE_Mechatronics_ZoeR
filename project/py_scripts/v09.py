b023eec4e9d4994dc1b5b0394dcff206aa342bac
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-08-20 15:05:29 +1000

    ultrasonic sense deez nuts (please help)

--- project/py_scripts/v09.py
+++ project/py_scripts/v09.py
@@ -1,17 +1,20 @@
-from subsystem_classes import servo_motor
+from subsystem_classes import servo_motor, ultrasonic_sensor
 from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
 from PiicoDev_Unified import sleep_ms
 
 
 servo_wheels = servo_motor(20, 18)
-range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-range_b = PiicoDev_Ultrasonic(id=[0, 0, 1, 0])
+ultrakill = ultrasonic_sensor([0, 0, 0, 0], True)
 
 while True:
-    if range_a.distance_mm > 100:
-        servo_wheels.slow_forward()
-        print(range_a.distance_mm)
+    if ultrakill.get_front() == None:
+        print("")
     else:
-        print("WE'RE GONNA CRASHHHHHHHH")
-        servo_wheels.stop()
-        servo_wheels.right()
+        print(ultrakill.get_front())
+        servo_wheels.slow_forward()
+        if ultrakill.get_front() > 100:
+            servo_wheels.slow_forward()
+        else:
+            print("WE'RE GONNA CRASHHHHHHHH")
+            servo_wheels.stop()
+            servo_wheels.right()
