42839608dd408724d31c9057f9207ce262769d0f
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-08-20 14:29:55 +1000

    movement code mostly works, might need some fine-tuning later

--- project/py_scripts/v09.py
+++ project/py_scripts/v09.py
@@ -8,9 +8,10 @@ range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
 range_b = PiicoDev_Ultrasonic(id=[0, 0, 1, 0])
 
 while True:
-    if range_a.distance_mm < 100:
-        print("WE'RE GONNA CRASHHHHHHHH")
-        servo_wheels.stop()
-    else:
+    if range_a.distance_mm > 100:
         servo_wheels.slow_forward()
         print(range_a.distance_mm)
+    else:
+        print("WE'RE GONNA CRASHHHHHHHH")
+        servo_wheels.stop()
+        servo_wheels.right()
