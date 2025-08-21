2658cac018d71b4513bef1323e1b62115eaf7eec
Zoe R <zoe.rath@education.nsw.gov.au>
2025-08-22 01:11:15 +1000

    Update v12.py

--- project/py_scripts/unit_tests/v12.py
+++ project/py_scripts/unit_tests/v12.py
@@ -1,7 +1,8 @@
-#colour sensor/LCD screen class unit tests
+#tests for colour/lcd code
+
 
 from colour_screen_classes import colour_lcd
 
 colour_lcd = colour_lcd(True)
-colour_lcd.print_screen()
+# colour_lcd.print_screen() <- old function
 colour_lcd.green_sensor()
